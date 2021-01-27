04ba67626156f43e71ac9d322839b60be06bbc9b
KiranLDA <kiran.dhanjal.adams@gmail.com>
2021-01-27 12:06:03 +0100

    added audiopool to data generator
    this means that as files are opened, they go into a cache and once the cache is filled, new files replace old open files. This speeds up code by not repeatedly opening and shutting files

--- model/specgen_batch_generator.py
+++ model/specgen_batch_generator.py
@@ -12,6 +12,7 @@ import lib.audioframes
 import lib.dftstream
 import lib.endpointer
 import preprocess.preprocess_functions as pre
+import model.audiopool as audiopool
 
 class DataGenerator(keras.utils.Sequence):
     def __init__(self, 
@@ -19,7 +20,6 @@ class DataGenerator(keras.utils.Sequence):
                  mega_table, #(rename so it is clear it is label)
                  mega_noise_table,  #(rename so it is clear it is label)
                  spec_window_size,
-                 # sample_size, #(method could be in data generator) 
                  window_size,
                  n_mels, window, fft_win , fft_hop , normalise,
                  label_for_noise,
@@ -47,7 +47,7 @@ class DataGenerator(keras.utils.Sequence):
         if other_ignored_in_training:
             del self.call_table_dict[label_for_other]
         
-        #setup indexing given different calls have different sample sizes        
+        # setup indexing given different calls have different sample sizes        
         self.indexes = dict()
         self.next_sample = dict()
         for calltype in call_table_dict.keys():
@@ -56,12 +56,18 @@ class DataGenerator(keras.utils.Sequence):
             np.random.shuffle(self.indexes[calltype]) 
             # create a variable which keeps track of samples
             self.next_sample[calltype] = 0
+            
+        # cache the audio data
+        self.pool = audiopool.AudioPool()  # Create audio pool   
         
-        # Q should this be here?
+        
+        # calculate sample size and batch size
         self.mean_sample_size, self.sample_size = self.sampling_strategy() # Q is it correct to add self?
         self.batch_size = self.n_per_call * len(self.call_table_dict.keys())
         self.tot_batch_number = int(np.floor(self.mean_sample_size / self.batch_size))
-        # Q shouldn't something about the total number of batches be here?
+        
+         
+        
     
     def __len__(self):
         """len() - Number of batches in data"""
@@ -89,9 +95,11 @@ class DataGenerator(keras.utils.Sequence):
                                                        (call["Start"]+call["End"])/2)), 3)
         
         # load in a subsection of the spectrogram
-        y, sr = librosa.load(call["wav_path"], sr=None, mono=False,
-                             offset = call_start, duration =self.spec_window_size)
-        
+        # y, sr = librosa.load(call["wav_path"], sr=None, mono=False,
+        #                      offset = call_start, duration =self.spec_window_size)
+        y = self.pool.get_seconds(call["wav_path"], call_start, self.spec_window_size)
+        sr = self.pool.get_Fs(call["wav_path"])
+            
         call_stop = round(call_start + self.spec_window_size,3 )
         
         # convert from a time to a framenumber
@@ -105,16 +113,21 @@ class DataGenerator(keras.utils.Sequence):
         if to_augment :           
             
             # extract noise call
-            noise_event = self.call_table_dict[self.label_for_noise].iloc[(self.indexes[self.label_for_noise][self.next_sample[self.label_for_noise]] )             
+            noise_event = self.call_table_dict[self.label_for_noise].iloc[self.indexes[self.label_for_noise][self.next_sample[self.label_for_noise]]]            
             # noise_event =  self.mega_noise_table.sample()
             
             #randomise the start and stop so the same section is never being used for the augmentation
-            noise_start = round(float(np.random.uniform(noise_event.iloc[0]["Start"], 
-                                                        (noise_event.iloc[0]["End"]-self.spec_window_size), 1)), 3)
-            noise_stop = round(noise_start + spec_window_size,3 )
-            y_noise, sr = librosa.load(noise_event["wav_path"], sr=None, mono=False,
-                                 offset = noise_start, duration =self.spec_window_size)
-            # start = int(round(sr * decimal.Decimal(noise_start),3))
+            noise_start = round(float(np.random.uniform(noise_event.loc["Start"], 
+                                                        (noise_event.loc["End"]-self.spec_window_size),
+                                                        1)),
+                                3)
+            noise_stop = round(noise_start + spec_window_size,3 )    
+        
+            y_noise = self.pool.get_seconds(noise_event["wav_path"], noise_start, self.spec_window_size)
+            sr = self.pool.get_Fs(noise_event["wav_path"])
+            # y_noise, sr = librosa.load(noise_event["wav_path"], sr=None, mono=False,
+            #                      offset = noise_start, duration =self.spec_window_size)
+            # # start = int(round(sr * decimal.Decimal(noise_start),3))
             # stop =  int(round(sr * decimal.Decimal(noise_stop),3))
             noise_subset = np.asfortranarray(y_noise)
     
@@ -152,6 +165,10 @@ class DataGenerator(keras.utils.Sequence):
         start_idx = batch_number * self.n_per_call
         stop_idx = (batch_number + 1) * self.n_per_call 
         '''
+              
+        
+        #########
+        
         # empty the batch at the beginning
         batch_label_data = []
         batch_spec_data = []
@@ -256,168 +273,3 @@ class DataGenerator(keras.utils.Sequence):
             
 
      
-'''
-
-
-import numpy as np
-import tensorflow.keras as keras
-import lib.audioframes
-import lib.dftstream
-import lib.endpointer
-
-class DataGenerator(keras.utils.Sequence):
-    def __init__(self, filenames, classes, batch_size=100, adv_ms=10, len_ms=20,
-                  duration_ms = 300, shuffle=True, flatten=True, cache=True):
-        """
-
-        :param filenames: list of filenames containing examples
-        :param classes:  class labels
-        :param batch_size:  number items in batch
-        :param adv_ms:  frame advance ms
-        :param len_ms:  frame length ms
-        :param duration_ms:  duration of speech to be classified
-        :param shuffle:  Randomize order of examples each epoch?  True/False
-        :param flatten:  Squash R^d features to R^1?  True/False
-        :param cache:  Cache feature generation?  True/False (not for big data)
-        """
-
-        self.epoch = 0   # starting epoch
-        self.N = len(filenames)
-        self.filenames = filenames
-        self.classes = np.array(classes)
-        self.batch_size = batch_size
-        # Store one-hot target encoding
-        self.targets = keras.utils.to_categorical(classes)
-
-        # framing parameters
-        self.adv_ms = adv_ms
-        self.len_ms = len_ms
-
-        # speech length to extract
-        self.duration_ms = duration_ms
-
-        # Data will be handled in this order (changed if shuffle True)
-        self.order = np.arange(self.N)
-
-        self.shuffle = shuffle
-        if self.shuffle:
-            np.random.shuffle(self.order)
-
-        self.flatten = flatten
-        if cache:
-            self.cache = dict()
-        else:
-            self.cache = None
-
-
-
-    def __len__(self):
-        """len() - Number of batches in data"""
-        return int(np.floor(self.N / self.batch_size))
-
-    def get_example(self, exampleidx):
-        """
-        get_example - Return features associated with a specific example
-        :param exampleidx: Compute features for self.filenames[exampleidx]
-        :param duration_ms:  How much data should be retained
-        :return: tensor to be returned to model
-        """
-
-        file = self.filenames[exampleidx]
-
-        if self.cache and file in self.cache:
-            features = self.cache[file]  # already computed, use it
-        else:
-            # First time, extract spectra
-            framer = lib.audioframes.AudioFrames(self.filenames[exampleidx],
-                                                  self.adv_ms, self.len_ms)
-            dftstream = lib.dftstream.DFTStream(framer)
-            spectra = [s for s in dftstream]
-            spectra = np.vstack(spectra)
-
-            # Find which examples are likely to be speech
-            ep = lib.endpointer.Endpointer(spectra)
-            indices = ep.speech_indices(spectra)
-
-            # Find midpoint of speech frames
-            # indices are sorted, so middle one is median frame
-            center_frame = indices[int(len(indices)/2)]
-
-            # Number of frames to use on either side of the center frame
-            # is half the duration divided by the frame rate
-            frame_offset = int(self.duration_ms/(2*self.adv_ms))
-
-            start_frame = center_frame - frame_offset
-            end_frame = center_frame + frame_offset+1
-
-            # Make sure we didn't go off either end and adjust if needed
-            if start_frame < 0:
-                start_frame = 0
-                end_frame = 2 * frame_offset + 1
-            elif end_frame > spectra.shape[0]:
-                end_frame = spectra.shape[0]
-                start_frame = end_frame - 2 * frame_offset - 1
-
-            # Extract featurs likely to contain the word
-            features = spectra[start_frame:end_frame, :]
-
-            if self.flatten:
-                features = features.flatten()
-
-            if self.cache:
-                self.cache[file] = features
-
-        return features
-
-    def get_labels(self):
-        """
-        get_labels() - Get category indices of all examples in set.
-        If the data have been shuffled, categories are ordered according to
-        the most recent shuffle and will no longer be valid after on_epoch_end()
-        is called.
-        :return:
-        """
-        if self.shuffle:
-            labels = self.classes[self.order]
-        else:
-            labels = self.classes
-
-        return labels
-
-
-
-    def __getitem__(self, batch_idx):
-        """
-        Get idx'th batch
-        :param batch_idx: batch number
-        :return: (examples, targets) - Returns tuple of data and targets for
-            specified batch
-        """
-
-        # Determine indices to process
-        start = batch_idx * self.batch_size
-        stop = start + self.batch_size  # one past last
-
-        examples = []
-        for idx in range(start, stop):
-            data = self.get_example(self.order[idx])
-            examples.append(data)
-
-        examples_tensor = np.stack(examples, axis=0)
-
-        targets = self.targets[self.order[start:stop],:]
-
-        return examples_tensor, targets
-
-
-
-    def on_epoch_end(self):
-        """
-        on_epoch_end - Bookkeeping at the end of the epoch
-        :return:
-        """
-        self.epoch += 1  # Note next epoch
-        if self.shuffle:
-            np.random.shuffle(self.order)  # reshuffle the data
-
-'''
